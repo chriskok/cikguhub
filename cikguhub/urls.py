427dc5c2530919996668454df8164cd9bf752628
chriskok <pherkok@gmail.com>
2023-02-19 09:14:08 -0500

    Added urls to apps from homepage

--- cikguhub/urls.py
+++ cikguhub/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recsys/', include('recsys.urls')),
-    path('report/', include('report.urls')),
+    path('recsys/', include(('recsys.urls', 'recsys'), namespace='recsys')),
+    path('report/', include(('report.urls', 'report'), namespace='report')),
+    path("", TemplateView.as_view(template_name='index.html'),
+        name='home'),
 ]
--- report/views.py
+++ report/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the report index.")
+    # return HttpResponse("Hello, world. You're at the report index.")
+
+    return render(request, "index.html")
