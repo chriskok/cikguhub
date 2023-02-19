b63fb55f9d331d41c42f83a1318c278e2903c8e5
chriskok <pherkok@gmail.com>
2023-02-19 09:57:01 -0500

    Added login functionality

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("register", views.register_request, name="register")
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
 ]
