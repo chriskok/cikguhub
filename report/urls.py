3163a35bb965aea7f7cc7367999e02f2041a6ce7
chriskok <pherkok@gmail.com>
2023-03-05 13:20:17 -0500

    Begun expert report page

--- report/urls.py
+++ report/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("user_report/", views.user_report, name="user_report"),
-    path("expert_report/", views.expert_report, name="expert_report"),
+    path("expert_report/<int:user_id>/", views.expert_report, name="expert_report"),
 ]
--- report/views.py
+++ report/views.py
@@ -17,31 +17,13 @@ def user_report(request):
     }
     return render(request, "report.html", context)
 
-def expert_report(request):
-    return render(request, "report_expert.html")
+def expert_report(request, user_id):
+    # Get the user in question
+    if (user_id == 0): curr_user = User.objects.first()
+    else: curr_user = User.objects.get(pk=user_id)
 
-# def user_recs(request):
-#     recommended_module_ids = json.loads(RecommendationQueue.objects.get(user=request.user).list_of_module_ids)
-#     if len(recommended_module_ids) > 0:
-#         recommended_module_id = recommended_module_ids[0]
-#         context = {"module": Module.objects.get(pk=recommended_module_id)}
-#     else:
-#         context = {"module": None}
+    print("HELKERKJDWA")
+    curr_learner_model = LearnerModel.objects.get(user=curr_user)
 
-#     return render(request, "recs.html", context=context)
-
-# def expert_recs(request, user_id):
-#     # TODO: add easily swappable modules IF same video
-#     # TODO: add drag and drop interface for easily switching module order
-
-#     # Get the user in question
-#     if (user_id == 0): curr_user = User.objects.first()
-#     else: curr_user = User.objects.get(pk=user_id)
-
-#     # Get recommended modules, perserving the order of the recommendation list
-#     list_of_module_ids = json.loads(RecommendationQueue.objects.get(user=curr_user).list_of_module_ids)
-#     preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(list_of_module_ids)])
-#     recommended_modules = Module.objects.filter(id__in=list_of_module_ids).order_by(preserved)
-
-#     context = {"modules": recommended_modules, "curr_user": curr_user, "all_users": User.objects.all()}
-#     return render(request, "recs_expert.html", context=context)
+    context = {"learner_model": curr_learner_model, "curr_user": curr_user, "all_users": User.objects.all()}
+    return render(request, "report_expert.html", context)
