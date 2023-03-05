c031bba05a71c50a4a74cdb5782c8c2fbdbb868d
chriskok <pherkok@gmail.com>
2023-03-05 10:22:29 -0500

    Finished all new models for backend

---
+++ OL_data.py
@@ -0,0 +1,76 @@
+# INSTITUTIONS
+{
+  "meta": {
+    "status": 200
+  },
+  "data": [
+    {
+      "id": "mylearningservices",
+      "url": "https://www.openlearning.com/mylearningservices",
+      "name": "OpenLearning Ambassador Progam (OLAP)"
+    },
+    {
+      "id": "edvolutionenterprise",
+      "url": "https://www.openlearning.com/edvolutionenterprise",
+      "name": "Edvolution Enterprise"
+    }
+  ]
+}
+
+# COURSES
+{
+  "meta": {
+    "status": 200
+  },
+  "data": [
+    {
+      "id": "63f3088263eb2aa87f5b3d77",
+      "url": "https://www.openlearning.com/edvolutionenterprise/courses/cikguhub/",
+      "title": "CikguHub",
+      "image": "https://2b5d50194958d0846249-8f2a854a837d270f565cb1918c6ed22f.ssl.cf4.rackcdn.com/categories/OL.jpg",
+      "self_paced": True
+    }
+  ]
+}
+
+{
+  "meta": {
+    "status": 200
+  },
+  "data": [
+    {
+      "id": "60f12f305c477a74643f7058",
+      "url": "https://www.openlearning.com/edvolutionenterprise/courses/applied-leadership-programme/",
+      "title": "Applied Leadership Programme",
+      "image": "https://openlearning-cdn.s3.amazonaws.com/course__edvolutionenterprise_courses_appliedleadershipprogramme__course-promo-image-1626430771.99.jpg",
+      "self_paced": True
+    }
+  ]
+}
+
+# CLASSES
+{
+  "meta": {
+    "status": 200
+  },
+  "data": [
+    {
+      "title": "Class of 2023",
+      "created_by": "61305c57faa91e7b0c49975e",
+      "id": "63f30885f5225a3e2e600095"
+    }
+  ]
+}
+
+{
+  "meta": {
+    "status": 200
+  },
+  "data": [
+    {
+      "title": "Class of 2021",
+      "created_by": "60c96b2d740cf8359f45fddd",
+      "id": "60f12f368462f8a192b14d36"
+    }
+  ]
+}
--- main/models.py
+++ main/models.py
@@ -1,6 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-# Create backend for hosted videos, questions per video, learner activity (responses to Qs or video watch time)
-# Create backend for learner model (competencies, existing feedback, human-approved boolean, user preferences in sign up form)
+class LearnerModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=1024, null=True, blank=True)
+    years_of_experience = models.FloatField(default=0.0)
+    role = models.CharField(max_length=4096, null=True, blank=True)
+    skill_interests = models.TextField(max_length=4096, null=True, blank=True)
+    other_interests = models.TextField(max_length=4096, null=True, blank=True)
 
+    planner_score = models.FloatField(default=0.0)
+    guardian_score = models.FloatField(default=0.0)
+    mentor_score = models.FloatField(default=0.0)
+    motivator_score = models.FloatField(default=0.0)
+    assessor_score = models.FloatField(default=0.0)
+
+    feedback = models.TextField(max_length=4096, null=True, blank=True)
+    human_approved = models.BooleanField(default=False)
+    human_edited = models.BooleanField(default=False)
+    feedback_edit_history = models.TextField(null=True, blank=True)
+
+class Video(models.Model):
+    title = models.CharField(max_length=4096)
+    tags = models.CharField(max_length=4096, help_text = "comma-separated list of tags")
+    url = models.CharField(max_length=1024, unique=True)
+
+    def __str__(self):
+        return "{}".format(self.title)
+
+class VideoQuestion(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    question = models.CharField(max_length=4096)
+    type = models.CharField(max_length=100)   # MCQ (multiple choice) or OEQ (open-ended)
+    possible_answers = models.CharField(max_length=4096, null=True, blank=True)
+
+    def __str__(self):
+        return "{}: {}".format(self.video, self.question)
+
+class VideoCompletion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    watchtime = models.FloatField(null=True, blank=True, default=0.0)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{} finished {}".format(self.user, self.video)
+
+class AnswerToVideoQuestion(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    question = models.ForeignKey(VideoQuestion, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=4096)
+
+    def __str__(self):
+        return "{}: {} - {}".format(self.video, self.question, str(self.id))
+
+class RecommendationQueue(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    list_of_video_ids = models.CharField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return "{}: {} ".format(self.user, self.list_of_video_ids)
