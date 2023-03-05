eda3b812bbda7fdd92df6ccafc22cf1d04250029
chriskok <pherkok@gmail.com>
2023-03-05 14:11:26 -0500

    Finished sample report generation for experts

---
+++ report/templatetags/report_extras.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def by_user(qset, user):
+    return qset.filter(user=user)
+
+@register.filter
+def get_first_answer(qset):
+    return qset.first().answer
--- report/views.py
+++ report/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from main.models import *
+import datetime
 
 def index(request):
     return render(request, "report.html")
@@ -18,12 +19,20 @@ def user_report(request):
     return render(request, "report.html", context)
 
 def expert_report(request, user_id):
+    # TODO: figure out what best to show the expert user to make decisions
+    # TODO: maybe we can use AI to generate the summary of feedback first, and have human look over
+    # TODO: maybe we can use AI to select the best answers that show growth in specific competencies -> highlight that for experts
+
     # Get the user in question
     if (user_id == 0): curr_user = User.objects.first()
     else: curr_user = User.objects.get(pk=user_id)
 
-    print("HELKERKJDWA")
     curr_learner_model = LearnerModel.objects.get(user=curr_user)
-
-    context = {"learner_model": curr_learner_model, "curr_user": curr_user, "all_users": User.objects.all()}
+    completed_modules = ModuleCompletion.objects.filter(user=curr_user).all()
+    context = {
+        "learner_model": curr_learner_model, 
+        "curr_user": curr_user, 
+        "all_users": User.objects.all(),
+        "completed_modules": completed_modules,
+    }
     return render(request, "report_expert.html", context)
