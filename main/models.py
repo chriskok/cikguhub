0a53f7831e8c092e8b26994dfd60c5a1074b4d91
chriskok <pherkok@gmail.com>
2023-03-05 08:53:06 -0500

    Added sample video embed and navbar updates

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+# Create backend for hosted videos, questions per video, learner activity (responses to Qs or video watch time)
+# Create backend for learner model (competencies, existing feedback, human-approved boolean, user preferences in sign up form)
+
