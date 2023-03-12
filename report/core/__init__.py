137fbf7b28ca983dc7173fc1847bcf2b1d5fdbbd
wendytao <wendytao@umich.edu>
2023-03-12 16:48:14 -0400

    update fronend -- image files

--- report/core/__init__.py
+++ report/core/__init__.py
@@ -5,27 +5,27 @@ metrics = {
     "planner": Metric(
         "Planner",
         "You are good at planning your work and setting goals.",
-        "fa-calendar-check",
+        "static/img/planner.png",
     ),
     "guardian": Metric(
         "Guardian",
         "You are good at keeping your work organized and on track.",
-        "fa-clipboard-check",
+        "static/img/guardian.png",
     ),
     "mentor": Metric(
         "Mentor",
         "You are good at helping others learn and grow.",
-        "fa-chalkboard-teacher",
+        "static/img/mentor.png",
     ),
     "motivator": Metric(
         "Motivator",
         "You are good at motivating others to do their best.",
-        "fa-award",
+        "static/img/motivator.png",
     ),
     "assessor": Metric(
         "Assessor",
         "You are good at assessing your own work and the work of others.",
-        "fa-clipboard-list",
+        "static/img/assessor.png",
     ),
 }
 
