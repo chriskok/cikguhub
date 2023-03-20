9b738245eac63b6e9d8e33e076d21034c51af263
wendytao <wendytao@umich.edu>
2023-03-20 11:06:25 -0400

    add imgs to user report

--- report/core/__init__.py
+++ report/core/__init__.py
@@ -7,27 +7,27 @@ metrics = {
     "planner": Metric(
         "Planner",
         "You are good at planning your work and setting goals.",
-        "static/img/planner.png",
+        "img/planner.png",
     ),
     "guardian": Metric(
         "Guardian",
         "You are good at keeping your work organized and on track.",
-        "static/img/guardian.png",
+        "img/guardian.png",
     ),
     "mentor": Metric(
         "Mentor",
         "You are good at helping others learn and grow.",
-        "static/img/mentor.png",
+        "img/mentor.png",
     ),
     "motivator": Metric(
         "Motivator",
         "You are good at motivating others to do their best.",
-        "static/img/motivator.png",
+        "img/motivator.png",
     ),
     "assessor": Metric(
         "Assessor",
         "You are good at assessing your own work and the work of others.",
-        "static/img/assessor.png",
+        "img/assessor.png",
     ),
 }
 
--- report/core/metrics.py
+++ report/core/metrics.py
@@ -1,13 +1,13 @@
 class Metric:
-    def __init__(self, name, description, icon):
+    def __init__(self, name, description, icon_url):
         self.name = name
         self.description = description
-        self.icon = icon
+        self.icon_url = icon_url
 
     def to_view(self, val):
         return {
             'name': self.name,
             'description': self.description,
-            'icon': self.icon,
+            'icon_url': self.icon_url,
             'value': val
         }
