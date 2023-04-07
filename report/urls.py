4e1113308052780e8d50f2c5a0fe534334c97ef8
chriskok <pherkok@gmail.com>
2023-04-07 10:01:37 -0400

    Added edit page for feedback

--- report/urls.py
+++ report/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path("user_report/", login_required(views.user_report), name="user_report"),
     path("expert_report/<int:user_id>/", login_required(views.expert_report), name="expert_report"),
+    path("update_feedback/<pk>", login_required(views.FeedbackUpdateView.as_view()), name="update_feedback"),
 ]
--- report/views.py
+++ report/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
 from main.models import *
 import datetime
 from . import core
@@ -42,3 +45,18 @@ def expert_report(request, user_id):
         'feedback_obj': Feedback.objects.filter(user=curr_learner_model.user).latest('id'),
     }
     return render(request, "report_expert.html", context)
+
+class FeedbackUpdateView(SuccessMessageMixin, UpdateView):
+    model = Feedback
+    fields = ['feedback', "planner_score", "guardian_score", "mentor_score", "motivator_score", "assessor_score"]
+    template_name = 'feedback_update.html'
+    success_message = 'Feedback updated successfully!'
+
+    def get_success_url(self):
+        return reverse_lazy('report:expert_report', kwargs={'user_id': self.object.user.id})
+
+    def form_valid(self, form):
+        self.object.human_edited = True
+        self.object.human_approved = True
+        self.object.save()
+        return super().form_valid(form) 
