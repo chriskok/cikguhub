093dfdc407f8936ba74ebe75f29b1b6748600de4
chriskok <pherkok@gmail.com>
2023-06-06 08:29:50 -0400

    Finished keyword extraction

--- report/views.py
+++ report/views.py
@@ -18,6 +18,7 @@ import re
 from io import BytesIO
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
 from sklearn.feature_extraction import text
+import pandas as pd
 
 from utils import chatgpt
 from .core import feedback_system_prompt
@@ -66,16 +67,68 @@ def get_user_topics(curr_user):
         text=re.sub("(\\d|\\W)+"," ",text)
         return text
 
-    all_answers = [pre_process(x) for x in list(AnswerToVideoQuestion.objects.filter(user=curr_user).values_list('answer', flat=True))]
-    print(all_answers)
-
+    all_answers = [pre_process(x) for x in list(AnswerToVideoQuestion.objects.filter(user=curr_user).values_list('answer', flat=True)) if len(x.split()) > 1]
+    if (len(all_answers) == 0): return None, None
     cv=CountVectorizer(max_df=0.85,stop_words="english")
     word_count_vector=cv.fit_transform(all_answers)
-
     tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
     tfidf_transformer.fit(word_count_vector)
+    # print(list(cv.vocabulary_.keys())[:10])
 
-    print(list(cv.vocabulary_.keys())[:10])
+    def sort_coo(coo_matrix):
+        tuples = zip(coo_matrix.col, coo_matrix.data)
+        return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
+    def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+        """get the feature names and tf-idf score of top n items"""
+        #use only topn items from vector
+        sorted_items = sorted_items[:topn]
+        score_vals = []
+        feature_vals = []
+        for idx, score in sorted_items:
+            fname = feature_names[idx]
+            #keep track of feature name and its corresponding score
+            score_vals.append(round(score, 3))
+            feature_vals.append(feature_names[idx])
+
+        #create a tuples of feature,score
+        #results = zip(feature_vals,score_vals)
+        results= {}
+        for idx in range(len(feature_vals)):
+            results[feature_vals[idx]]=score_vals[idx]
+        
+        return results
+    
+    # you only needs to do this once
+    feature_names=cv.get_feature_names_out()
+    tf_idf_vector=tfidf_transformer.transform(cv.transform(all_answers))
+
+    results=[]
+    for i in range(tf_idf_vector.shape[0]):
+        # get vector for a single document
+        curr_vector=tf_idf_vector[i]
+        #sort the tf-idf vector by descending order of scores
+        sorted_items=sort_coo(curr_vector.tocoo())
+        #extract only the top n; n here is 10
+        keywords=extract_topn_from_vector(feature_names,sorted_items,10)
+        results.append(keywords)
+
+    df=pd.DataFrame(zip(all_answers,results),columns=['doc','keywords'])
+
+    # combine all keyword weight dicts in df into one dict
+    all_keywords = {}
+    for i in range(len(df)):
+        for key in df['keywords'][i]:
+            if key in all_keywords: all_keywords[key] += df['keywords'][i][key]
+            else: all_keywords[key] = df['keywords'][i][key]
+    
+    # sort the dict by value and return top 10
+    selected_keywords = dict(sorted(all_keywords.items(), key=lambda item: item[1], reverse=True)[:10])
+
+    # convert df keywords column to joint string
+    df['keywords'] = df['keywords'].apply(lambda x: ", ".join(list(x.keys())))
+
+    return ", ".join(list(selected_keywords.keys())), df.values.tolist()
 
 def user_report(request, user_id=0):
     if (user_id):
@@ -94,6 +147,7 @@ def user_report(request, user_id=0):
 
     curr_learner_model = LearnerModel.objects.get(user=curr_user)
     curr_feedback = Feedback.objects.filter(user=curr_learner_model.user).latest('id').feedback if Feedback.objects.filter(user=curr_learner_model.user).exists() else None
+    keywords, topics_by_answer = get_user_topics(curr_user)
     context = {
         'learner_model': curr_learner_model,
         'metrics': {
@@ -108,8 +162,9 @@ def user_report(request, user_id=0):
         "multimedia_plot": produce_plot('multimedia', curr_user),
         "coaching_plot": produce_plot('coaching', curr_user),
         "digital_plot": produce_plot('digital', curr_user),
+        "keywords": keywords,
+        "topics_by_answer": topics_by_answer,
     }
-    get_user_topics(curr_user)
     return render(request, "report.html", context)
 
 
@@ -126,6 +181,7 @@ def expert_report(request, user_id):
 
     curr_learner_model = LearnerModel.objects.get(user=curr_user)
     completed_modules = ModuleCompletion.objects.filter(user=curr_user).all()
+    keywords, topics_by_answer = get_user_topics(curr_user)
     context = {
         "learner_model": curr_learner_model,
         "curr_user": curr_user,
@@ -141,6 +197,8 @@ def expert_report(request, user_id):
         "multimedia_plot": produce_plot('multimedia', curr_user),
         "coaching_plot": produce_plot('coaching', curr_user),
         "digital_plot": produce_plot('digital', curr_user),
+        "keywords": keywords,
+        "topics_by_answer": topics_by_answer,
     }
     return render(request, "report_expert.html", context)
 
